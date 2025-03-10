fbb82d40f0ea33f46dc7873bae5aeb5af4a759bb
Neville <lemein.neville@gmail.com>
2025-03-10 03:07:52 +0300

    Fix

--- src/app/(app)/users/customers/corporate/view/page.tsx
+++ src/app/(app)/users/customers/corporate/view/page.tsx
@@ -1,8 +1,10 @@
-export default async function Page({
-  searchParams,
-}: Promise<{
-  id?: string;
-}>) {
+interface SearchParams extends Record<string, string | string[] | undefined> {}
+
+interface PageProps {
+  searchParams: Promise<SearchParams>;
+}
+
+export default async function Page({ searchParams }: PageProps) {
   const { id } = await searchParams;
 
   return <></>;
