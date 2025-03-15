07cb73e7555ad85a7315bb1dee22170787326d9d
Neville <lemein.neville@gmail.com>
2025-03-15 12:12:04 +0300

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,10 +8,35 @@ const ALLOWED_EMAIL_DOMAINS = ["kilimolink.com"];
 // Define public routes that don't require authentication
 const PUBLIC_ROUTES = ["/auth/login"];
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
+export async function middleware(request: NextRequest) {
+  await updateSession(request);
 
-  return res;
+  const url = new URL(request.url);
+
+  const pathname = url.pathname;
+
+  let supabase = await createClient();
+
+  let {
+    data: { user },
+  }: any = await supabase.auth.getUser();
+
+  if (!user) {
+    return Response.redirect(
+      new URL(`/login?message=Please Login to Access this Page`, request.url)
+    );
+  } else {
+    if (user?.user_metadata?.user_type !== "staff") {
+      await supabase.auth.signOut();
+      return Response.redirect(
+        new URL("/login?message=Invalid Request", request.url)
+      );
+    }
+
+    if (user?.user_metadata?.status !== "active") {
+      return Response.redirect(new URL("/pending-approval", request.url));
+    }
+  }
 }
 
 export const config = {
