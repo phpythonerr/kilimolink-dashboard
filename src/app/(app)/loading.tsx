bcee6617caa82f6ac886c032a6f74a1ed9e0155c
Neville <lemein.neville@gmail.com>
2025-03-15 19:27:10 +0300

    Add skeleton loader

---
+++ src/app/(app)/loading.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function Loading() {
+  return (
+    <div className="flex flex-col space-y-3">
+      <div className="space-y-2">
+        {[...Array(10)].map((item: any, i: number) => (
+          <Skeleton className="h-4 w-full" />
+        ))}
+      </div>
+    </div>
+  );
+}
