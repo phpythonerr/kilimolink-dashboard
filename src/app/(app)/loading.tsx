3f626d0204cfe2bf4aa5d4f9b4cffe4cea296e40
Neville <lemein.neville@gmail.com>
2025-03-15 19:27:36 +0300

    Export default in loading file

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-export function Loading() {
+export default function Loading() {
   return (
     <div className="flex flex-col space-y-3">
       <div className="space-y-2">
