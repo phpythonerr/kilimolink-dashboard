013805b99da8a24c2fa49633a01cdbf97fcaa68b
Neville <lemein.neville@gmail.com>
2025-03-29 23:52:59 +0300

    Fix

--- src/app/(app)/dashboard/@revenue/page.tsx
+++ src/app/(app)/dashboard/@revenue/page.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { formatCurrency } from "@/lib/utils";
 
 export default async function Revenue() {
   // Fetch your revenue data here
@@ -10,9 +9,7 @@ export default async function Revenue() {
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
       </CardHeader>
-      <CardContent>
-        <div className="text-2xl font-bold">Ksh.{formatCurrency(revenue)}</div>
-      </CardContent>
+      <CardContent></CardContent>
     </Card>
   );
 }
