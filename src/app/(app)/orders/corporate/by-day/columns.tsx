45c52bc97a933373a5aa65b8f451c685ceb046ee
Neville <lemein.neville@gmail.com>
2025-03-29 14:41:11 +0300

    Fix

--- src/app/(app)/orders/corporate/by-day/columns.tsx
+++ src/app/(app)/orders/corporate/by-day/columns.tsx
@@ -6,11 +6,6 @@ export interface OrderData {
   summary_date: string; // The date of the orders
   orders_count: number; // Number of orders
   total_amount?: number; // Total sales (optional)
-  total_selling_price?: number; // Total buying price (optional)
-  total_buying_price?: number; // Other revenues (optional)
-  total_revenue?: number; // Expenses (optional)
-  total_expenses?: number; // Expenses (optional)
-  profits?: number; // Profit (optional)
 }
 
 // Define your columns
@@ -47,39 +42,4 @@ export const columns: ColumnDef<OrderData>[] = [
       return `Ksh.${Number(row.getValue("total_amount")).toLocaleString()}`;
     },
   },
-  {
-    accessorKey: "total_buying",
-    header: "Total Buying",
-    cell: ({ row }) => {
-      const { total_buying_price } = row.original;
-      return `Ksh.${Number(total_buying_price).toLocaleString()}`;
-    },
-  },
-  {
-    accessorKey: "total_revenue",
-    header: "Other Revenues",
-    cell: ({ row }) => {
-      const { total_revenue } = row.original;
-      return `Ksh.${Number(total_revenue).toLocaleString()}`;
-    },
-  },
-  {
-    accessorKey: "total_expenses",
-    header: "Expenses",
-    cell: ({ row }) => {
-      const { total_expenses } = row.original;
-      return `Ksh.${Number(total_expenses).toLocaleString()}`;
-    },
-  },
-  // {
-  //   accessorKey: "profits",
-  //   header: "Profit",
-  //   cell: ({ row }) => {
-  //     const { total_selling_price, total_buying_price, revenue, expenses } =
-  //       row.original;
-  //     const profit =
-  //       total_selling_price + revenue - (total_buying_price + expenses);
-  //     return `Ksh.${Number(profit).toLocaleString()}`;
-  //   },
-  // },
 ];
