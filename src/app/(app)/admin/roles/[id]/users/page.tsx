6c8b0403609596062831e45dee2654e947837281
Neville <lemein.neville@gmail.com>
2025-04-13 10:24:48 +0300

    fix

--- src/app/(app)/admin/roles/[id]/users/page.tsx
+++ src/app/(app)/admin/roles/[id]/users/page.tsx
@@ -64,7 +64,7 @@ export default async function UsersWithRolePage({
     data: usersWithRole,
     count,
     error,
-  } = await supabase
+  }: any = await supabase
     .from("user_roles")
     .select(
       `
